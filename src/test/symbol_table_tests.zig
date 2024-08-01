e924818aa024590bd4dd8ce2119c0ce2049dd6c4
Jeremiah D. Oard <64376668+MiahDrao97@users.noreply.github.com>
2024-08-01 08:54:00 -0600

    more symbol table tests

--- src/test/symbol_table_tests.zig
+++ src/test/symbol_table_tests.zig
@@ -7,10 +7,18 @@ const Token = parsing.Token;
 const types = @import("game_zones").types;
 const DamageType = types.DamageType;
 const ExpressionResult = parsing.ExpressionResult;
+const FunctionDef = parsing.FunctionDef;
 const SymbolTable = parsing.SymbolTable;
 const Symbol = parsing.Symbol;
 
-test {
+fn testFunc(args: []ExpressionResult) !ExpressionResult {
+    if (args.len < 1) {
+        return ExpressionResult.void;
+    }
+    return args[0];
+}
+
+test "SymbolTable.putValue()" {
     {
         var symbol_table: SymbolTable = try SymbolTable.new(testing.allocator);
         defer symbol_table.deinit();
@@ -85,5 +93,64 @@ test {
 
         // scope is gone
         try testing.expect(symbol_table.getSymbol("$") == null);
+    }
+}
+test "SymbolTable.putFunc()" {
+    {
+        var symbol_table: SymbolTable = try SymbolTable.new(testing.allocator);
+        defer symbol_table.deinit();
+        
+        try symbol_table.putFunc("testFunc", &testFunc);
+
+        const symbol_value: ?Symbol = symbol_table.getSymbol("testFunc");
+        try testing.expect(symbol_value != null);
+
+        switch (symbol_value.?) {
+            Symbol.function => { },
+            else => return error.UnexpectedSymbol
+        }
+    }
+    {
+        var symbol_table: SymbolTable = try SymbolTable.new(testing.allocator);
+        defer symbol_table.deinit();
+        
+        try symbol_table.putFunc("testFunc", &testFunc);
+        const expr_value: ExpressionResult = .{
+            .integer = .{
+                .value = 3
+            }
+        };
+        try symbol_table.putValue("$", expr_value);
+
+        const func_symbol: ?Symbol = symbol_table.getSymbol("testFunc");
+        try testing.expect(func_symbol != null);
+
+        var func: FunctionDef = undefined;
+        switch (func_symbol.?) {
+            Symbol.function => |f| {
+                func = f;
+            },
+            else => return error.UnexpectedSymbol
+        }
+
+        const value_symbol: ?Symbol = symbol_table.getSymbol("$");
+        try testing.expect(value_symbol != null);
+
+        var val: ExpressionResult = undefined;
+        switch (value_symbol.?) {
+            Symbol.value => |v| {
+                val = v.*;
+            },
+            else => return error.UnexpectedSymbol
+        }
+
+        var args = [_]ExpressionResult { val };
+        const result: ExpressionResult = try func(&args);
+        switch (result) {
+            ExpressionResult.integer => |i| {
+                try testing.expect(i.value == 3);
+            },
+            else => return error.UnexpectedValue,
+        }
     }
 }
